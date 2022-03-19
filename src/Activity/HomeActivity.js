2eb4f7b2a081068aadc98057ddffd385de076472
Udhayakumar T <dev.udhayakumar@gmail.com>
2022-03-19 08:23:39 +0530

    datepicker added

--- src/Activity/HomeActivity.js
+++ src/Activity/HomeActivity.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import Card from '../components/card';
-import { NativeBaseProvider, Box, Select, CheckIcon, HStack } from "native-base";
+import { NativeBaseProvider, Pressable ,ChevronDownIcon,Text, Button, Box, Select, CheckIcon, HStack } from "native-base";
 import { ScrollView, Image, StyleSheet, } from 'react-native';
 import firestore from '@react-native-firebase/firestore';
-
+import DatePicker from 'react-native-date-picker'
 
 const HomeActivity = ({ navigation }) => {
     const [workstation, setWorkStation] = useState("");
@@ -12,7 +12,10 @@ const HomeActivity = ({ navigation }) => {
     const [updateWorkStation, setUpdateWorkStation] = useState([]);
     const [updateSubStation, setUpdateSubStation] = useState([]);
     const [querydata, setquerydata] = useState([]);
-
+    const [pickdate, setpickDate] = useState(new Date);
+    const [date , setdate] = useState(pickdate.getFullYear()+"-"+(pickdate.getMonth()+1)+"-"+pickdate.getDate());
+    
+    const [open, setOpen] = useState(false);
 
     useEffect(() => {
         GetData();
@@ -52,15 +55,16 @@ const HomeActivity = ({ navigation }) => {
     async function getAllData() {
         const formValues = [];
         var today = new Date();
-        let month = today.getMonth() + 1;
-        let tod = today.getDate();
+        let month = pickdate.getMonth()+1;
+        let tod = pickdate.getDate();
+        console.log(tod + " " + month);
         if (tod < 10) {
             tod = "0" + tod;
         }
         if (month < 10) {
             month = "0" + month;
         }
-        var collection = "2022-03-02";
+        var collection = pickdate.getFullYear()+"-"+month+"-"+tod;
         await firestore()
             .collection(collection).where("substation", "==", substation).where("workstation", "==", workstation)
             .get()
@@ -81,9 +85,9 @@ const HomeActivity = ({ navigation }) => {
                     source={{
                         uri: 'https://www.vguard.in/waterpurifier/assets/images/logo.png',
                     }} />
-                <HStack space={2} mb="10" justifyContent="center">
+                <HStack space={2} mb="3" justifyContent="center">
                     <Box w={{ base: "45%" }}>
-                        <Select selectedValue={workstation} minWidth="30%" accessibilityLabel="Choose WorkStation" placeholder="Choose WorkStation" _selectedItem={{
+                        <Select bg="#fff" selectedValue={workstation} minWidth="30%" accessibilityLabel="Choose WorkStation" placeholder="Choose WorkStation" _selectedItem={{
                             bg: "teal.600",
                             endIcon: <CheckIcon size="5" />
                         }} mt={1} onValueChange={(itemValue) => {
@@ -99,12 +103,13 @@ const HomeActivity = ({ navigation }) => {
                         </Select>
                     </Box>
                     <Box w={{ base: "45%" }} >
-                        <Select selectedValue={substation} minWidth="50%" accessibilityLabel="Choose Model" placeholder="Choose Service" _selectedItem={{
+                        <Select bg="#fff" selectedValue={substation} minWidth="50%" accessibilityLabel="Choose Model" placeholder="Choose Service" _selectedItem={{
                             bg: "teal.600",
                             endIcon: <CheckIcon size="5" />
-                        }} mt={1} onValueChange={(itemValue) =>{
+                        }} mt={1} onValueChange={(itemValue) => {
                             setsubStation(itemValue)
-                            getAllData()}
+                            getAllData()
+                        }
                         }>
                             <Select.Item key={0} label="All" value="All" />
                             {
@@ -115,13 +120,36 @@ const HomeActivity = ({ navigation }) => {
                         </Select>
                     </Box>
                 </HStack>
+                <Pressable onPress={() => setOpen(true)}>
+                    <Box ml="4" mb="5" h="8" rounded="sm" w="92%">
+                    <HStack>
+                    <Text m="1" w="90%">{date.toString()}</Text>
+                    <ChevronDownIcon w="10%" m="1" size="5" />
+                    </HStack>
+                    </Box>
+                </Pressable>
+                <DatePicker
+                    modal
+                    open={open}
+                    date={pickdate}
+                    mode="date"
+                    onConfirm={(date) => {
+                        setOpen(false)
+                        setpickDate(date)
+                        setdate(date.getFullYear()+"-"+(date.getMonth()+1)+"-"+date.getDate())
+                        getAllData()
+                    }}
+                    onCancel={() => {
+                        setOpen(false)
+                    }}
+                />
                 <ScrollView>
                     {
-                        querydata.map((element,index)=>(
-                            <Card data={element} key={index}/>
+                        querydata.map((element, index) => (
+                            <Card data={element} key={index} />
                         ))
                     }
-                   
+
                 </ScrollView>
             </NativeBaseProvider>
 
